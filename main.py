2123eeb394eb358ff2107d3f2dafff96ed4d27d3
Dalen <63216157+dalencoso@users.noreply.github.com>
2021-08-26 04:48:25 -0400

    Update main.py
    Updated the main file so tips actually get calculated

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+import math
+
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
 
 app = Flask(__name__)
 
@@ -25,19 +30,32 @@ def add():
     if request.method=='GET':
         return render_template("add.html")
     if request.method=='POST':
-        names = request.form.getlist('textbox')
-        return render_template('addresults.html', names=names)
+        names = request.form.getlist('names')
+        hours = request.form.getlist('hoursworked')
+        totaltips = request.form['tips']
+        totalhours = 0.0
+        for hour in hours:
+            totalhours = totalhours + float(hour)
+
+        tipsperhour = float(totaltips)/totalhours
+
+        personaltips = []
+        count = 0
+        for a in range(len(names)):
+            personaltips.append(round_down(tipsperhour * float(hours[a]), 2))
+
+        data = zip(names, personaltips)
+        data = list(data)
+
+
+        return render_template('addresults.html', data=data, totalhours=totalhours, totaltips=totaltips)
 
 @app.route('/addresults', methods=["POST", "GET"])
 def results():
     if request.method=='POST':
         names = request.form['textbox']
-        return render_template('addresults.html', names=names)
+        return render_template('addresults.html', names=names, tips=tips)
     return render_template("addresults.html")
-
-@app.route('/api/<name>/<token>')
-def api(name, token):
-    return f"<h1>{name} {token}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
